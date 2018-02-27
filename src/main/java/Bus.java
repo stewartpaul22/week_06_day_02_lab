2b34ee7e9ff3906e263283a552a9a4bb8e016625
Paul Stewart <stewartpaul22@yahoo.co.uk>
2018-02-27 13:44:57 +0000

    Add passengerCount() method to Bus class and corresponding test.

--- src/main/java/Bus.java
+++ src/main/java/Bus.java
@@ -4,6 +4,17 @@ public class Bus {
 
     private String destination;
     private int capacity;
-    private ArrayList<person> passengers;
+    private ArrayList<Person> passengers;
+
+    public Bus(String destination, int capacity) {
+        this.destination = destination;
+        this.capacity = capacity;
+        this.passengers = new ArrayList<Person>();
+    }
+
+    public int passengerCount() {
+        return this.passengers.size();
+    }
+
 
 }
--- src/test/java/BusTest.java
+++ src/test/java/BusTest.java
@@ -1,5 +1,20 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BusTest {
 
+    Bus bus;
 
+    @Before
+    public void before() {
+        bus = new Bus("Edinburgh", 30);
+    }
+
+    @Test
+    public void busStartsEmpty() {
+        assertEquals(0, bus.passengerCount());
+    }
 
 }
